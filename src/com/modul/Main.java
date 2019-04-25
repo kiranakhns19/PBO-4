40d950bcac648e1644e6c72c80bec16dcaf9e04e
Mufidzz <dmzmzzz@users.noreply.github.com>
2019-04-25 23:15:04 +0700

    Repair Main Class

--- src/com/modul/Main.java
+++ src/com/modul/Main.java
@@ -1,17 +1,14 @@
 package com.modul;
 
 public class Main {
-
     public static void main(String[] args) {
-    AbstractShape L = new Lingkaran();
-	    System.out.println("Lingkaran : ");
-	    ((Lingkaran) L).getLuas();
-	    ((Lingkaran) L).getKeliling();
+        AbstractShape L = new Lingkaran();
+        AbstractShape P = new Persegi();
 
-	AbstractShape P = new Persegi();
-        System.out.println("Persegi : ");
-        ((Persegi) P).getLuas();
-        ((Persegi) P).getKeliling();
+        System.out.println("Luas Lingkaran : " + L.getLuas());
+        System.out.println("Keliling Lingkaran : " + L.getKeliling());
 
+        System.out.println("Luas Persegi : " + P.getLuas());
+        System.out.println("Keliling Persegi : " + P.getKeliling());
     }
 }
